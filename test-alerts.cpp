e36c658fefd0b8664f371b8d4795f42f7500ac6d
Sana Srikar (RBEI/ESP-IS14) <Srikar.Sana@in.bosch.com>
2021-04-03 07:36:01 +0530

    breachtypetostringconversion testcase

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -3,6 +3,7 @@
 #include "test/catch.hpp"
 #include "test_stub.h"
 #include "typewise-alert.h"
+#include "string.h"
 
 TEST_CASE("infers the TOO_LOW breach") {
   REQUIRE(inferBreach(12.0, 20.0, 30.0) == TOO_LOW);
@@ -20,6 +21,8 @@ TEST_CASE("infers the NORMAL : Value equal to upper limit") {
   REQUIRE(inferBreach(30.0, 20.0, 30.0) == NORMAL);
 }
 
+
+/* using philosophy of art of readable code */
 TEST_CASE("Check if Alert is being called in Check and Alert with inferbreach return value")
 {
   BatteryCharacter battery;
@@ -28,3 +31,15 @@ TEST_CASE("Check if Alert is being called in Check and Alert with inferbreach re
   REQUIRE(GetCall_AlertTestDouble() == 1);
   REQUIRE(GetPar_breach() == TOO_HIGH);
 }
+
+TEST_CASE("infers the NORMAL : Value equal to upper limit") {
+  REQUIRE(inferBreach(30.0, 20.0, 30.0) == NORMAL);
+}
+
+TEST_CASE("Evaluate BreachTypeToString conversion") {
+  REQUIRE(strcmp(BreachTypeToString[TOO_LOW],"too low") == 0);
+  REQUIRE(strcmp(BreachTypeToString[TOO_HIGH],"too high") == 0);
+  REQUIRE(strcmp(BreachTypeToString[NORMAL],"normal") == 0);
+}
+
+
